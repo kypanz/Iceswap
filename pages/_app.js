df4d489db11e6f96d7fccd7a59c2956edcbc2748
kypanz <kypanz@gmail.com>
2022-08-18 22:15:48 -0300

    adding moralis integration for the nextjs dapp

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,19 @@
 import '../styles/globals.css';
 import '../styles/App.css';
 import 'bootstrap/dist/css/bootstrap.css';
-
+import { MoralisProvider } from "react-moralis";
 import Layout from '../components/Layout';
 
 function MyApp({ Component, pageProps }) {
   return (
+    <MoralisProvider
+      appId={process.env.NEXT_PUBLIC_APP_ID}
+      serverUrl={process.env.NEXT_PUBLIC_SERVER_URL}
+    >
     <Layout>
       <Component {...pageProps} />
     </Layout>
+    </MoralisProvider>
   )
 }
 
